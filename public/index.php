ac53c3113e542322cc84b2fce7a7a60eff283efb
Bluebom <franklinhpf@hotmail.com>
2021-06-15 21:47:54 -0300

    inicio do projeto

---
+++ public/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <section class="container">
+      <div class="grid-2">
+        <h2>LOGO</h2>
+      </div>
+      <div class="right grid-4">
+        <nav>
+          <ul>
+            <li><a href="#" class="ativo">Home</a></li>
+            <li><a href="#">Cadastro</a></li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  </header>
+  <section class="lutadores">
+    <section class="container">
+      <div class="titulos grid-1-3 selecionado">
+        <h3>Leve</h3>
+      </div>
+      <div class="titulos grid-1-3">
+        <h3>Meio-Pesado</h3>
+      </div>
+      <div class="titulos grid-1-3">
+        <h3>Pesado</h3>
+      </div>
+    </section>
+    <!-- container -->
+    <section clas="container">
+      <div class="lutador grid-12">Lutador 01</div>
+      <div class="lutador grid-12">Lutador 02</div>
+      <div class="lutador grid-12">Lutador 03</div>
+      <div class="lutador grid-12">Lutador 04</div>
+      <div class="seta grid-2">Arrow left</div>
+      <div class="seta grid-2">Arrow right</div>
+    </section>
+  </section>
+  <!-- fighters -->
+  <footer></footer>
+</body>
+
+</html>
