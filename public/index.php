2060a2269d1f7f227ca5215a41d72778a246df8d
Bluebom <franklinhpf@hotmail.com>
2021-06-16 21:36:26 -0300

    released home page

--- public/index.php
+++ public/index.php
@@ -25,6 +25,7 @@
       </div>
     </section>
   </header>
+  <h1>Lutadores</h1>
   <section class="lutadores">
     <section class="container">
       <div class="titulos grid-1-3 selecionado">
@@ -38,13 +39,42 @@
       </div>
     </section>
     <!-- container -->
-    <section clas="container">
-      <div class="lutador grid-12">Lutador 01</div>
-      <div class="lutador grid-12">Lutador 02</div>
-      <div class="lutador grid-12">Lutador 03</div>
-      <div class="lutador grid-12">Lutador 04</div>
-      <div class="seta grid-2">Arrow left</div>
-      <div class="seta grid-2">Arrow right</div>
+    <section class="container noRow">
+      <div class="lutador grid-12">
+        <img src="../assets/image/eubg-removebg-preview.png" alt="foto do lutador">
+        <article>
+          <h4 class="nome">Franklin Henrique</h4>
+          <hr>
+          <p>Quantidade de vitórias:</p>
+          <p>Quantidade de derrotas:</p>
+          <p>Quantidade de empates:</p>
+          <hr>
+        </article>
+      </div>
+      <div class="lutador grid-12">
+        <img src="../assets/image/eubg-removebg-preview.png"" alt=" foto do lutador">
+        <article>
+          <h4 class="nome">Franklin Henrique</h4>
+          <hr>
+          <p>Quantidade de vitórias:</p>
+          <p>Quantidade de derrotas:</p>
+          <p>Quantidade de empates:</p>
+          <hr>
+        </article>
+      </div>
+      <div class="lutador grid-12">
+        <img src="../assets/image/eubg-removebg-preview.png"" alt=" foto do lutador">
+        <article>
+          <h4 class="nome">Franklin Henrique</h4>
+          <hr>
+          <p>Quantidade de vitórias:</p>
+          <p>Quantidade de derrotas:</p>
+          <p>Quantidade de empates:</p>
+          <hr>
+        </article>
+      </div>
+      <div class="seta left grid-2"></div>
+      <div class="seta right grid-2"></div>
     </section>
   </section>
   <!-- fighters -->
