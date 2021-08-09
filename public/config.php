ca995e4344336751097c9ba27f9972676de0debb
Bluebom <Franklinhpf@hotmail.com>
2021-08-08 22:18:25 -0300

    atualizacao de config

--- public/config.php
+++ public/config.php
@@ -7,10 +7,14 @@ $autoload = function ($class) {
 
 spl_autoload_register($autoload);
 
-define('INCLUD_PATH', 'http://192.168.2.119/dashboard/projetoUFCE/public/');
-define('BASE__DIR__IMG', __DIR__ . '/../assets/image/');
+define('INCLUD_PATH', 'https://ufce.franklinhenrique.com/');
+define('BASE__DIR__IMG', __DIR__ . '/assets/image/');
 
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'UFCE');
+//define('HOST', 'localhost');
+//define('USER', 'root');
+//define('PASSWORD', '');
+//define('DATABASE', 'UFCE');
+define('HOST', '192.185.176.109');
+define('USER', 'frankl04_proj');
+define('PASSWORD', '8aca7EAE!!');
+define('DATABASE', 'frankl04_UFCE');
